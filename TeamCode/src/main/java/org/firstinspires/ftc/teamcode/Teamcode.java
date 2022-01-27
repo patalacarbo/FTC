9f9194ca7ad3f15daff5c637be70567a8cafedfc
carbo <tetihusseini8@gmail.com>
2022-01-27 17:22:52 +0100

    Second commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teamcode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class Teamcode {
+
+}
